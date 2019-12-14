89acfd3f1b80eb89c88e4bcbbf6512e40ba8ab60
Samuel-DEVULDER <samuel.devulder@laposte.net>
2019-12-14 15:52:01 +0100

    speedup smk with a bit of 68k asm.

--- 3rdParty/libsmacker/smk_bitstream.c
+++ 3rdParty/libsmacker/smk_bitstream.c
@@ -48,9 +48,10 @@ error:
 	return ret;
 }
 
-int _smk_error(struct smk_bit_t* bs)
+unsigned char _smk_error(struct smk_bit_t* bs)
 {
-	fprintf(stderr, "libsmacker::_smk_bs_read_1(bs): ERROR: bitstream (length=%lu) exhausted.\n", bs->siz);
+	fprintf(stderr, "libsmacker::_smk_bs_read_?(bs=%p): ERROR: bitstream (length=%lu, ptr=%p, end=%p) exhausted.\n", bs, bs->siz, bs->ptr, bs->end);
+	bs->buf=1;
 	return -1;
 }
 
@@ -70,7 +71,7 @@ char _smk_bs_read_1(struct smk_bit_t* bs)
 	"	bne.b	.lab%=		\n"
 	"	move.l	2(a0),a1	\n"
 	"	cmp.l	6(a0),a1	\n"
-	"	bcc.w	__smk_error	\n"
+	"	bcc.b	__smk_error	\n"
 	"	move.w	#256,d1		\n"
 	"	move.b	(a1)+,d1	\n"
 	"	lsr.w	#1,d1		\n"
@@ -79,7 +80,7 @@ char _smk_bs_read_1(struct smk_bit_t* bs)
 	".lab%=:				\n"
 	"	addx.l	d0,d0		\n"
 	: "=d" (ret) : "a" (bs_)
-	: );
+	: "d1","a1","a0" );
 #else
 	unsigned short ret;
 
@@ -107,15 +108,15 @@ short _smk_bs_read_8(struct smk_bit_t* bs)
 	__asm__ __volatile__ (
 	"	move.l	2(a0),a1	\n"
 	"	cmp.l	6(a0),a1	\n"
-	"	bcc.w	__smk_error	\n"
+	"	bcc.b	__smk_error	\n"
 	"	addq.l	#1,2(a0)	\n"
 	"	moveq	#0,d0		\n"
 	"	moveq	#1,d1		\n"
 	"	move.w	(a0),d0		\n"
 	"	cmp.w	d1,d0		\n"
-	"	bhi		.lab%=		\n"
+	"	bhi.b	.lab%=		\n"
 	"	move.b	(a1),d0		\n"
-	"	rts					\n"
+	"	bra.b	.xit%=		\n"
 	".lab%=:				\n"
 	"	swap	d1			\n"
 	"	move.w	(a1),d1     \n"
@@ -124,11 +125,13 @@ short _smk_bs_read_8(struct smk_bit_t* bs)
 	"	sub.w	#23,d0		\n"
 	"	lsl.b	d0,d1		\n"
 	"	lsr.l	d0,d1		\n"
+	// " moveq #0,d0\n" // necessaire ????
 	"	move.b	d1,d0		\n"
 	"	lsr.l	#8,d1		\n"
 	"	move.w	d1,(a0)		\n"
+	".xit%=:				\n"
 	: "=d" (ret) : "a" (bs_)
-	: );
+	: "d1","a1","a0");
 #else
 	unsigned char ret; unsigned short a;
 
@@ -140,7 +143,7 @@ short _smk_bs_read_8(struct smk_bit_t* bs)
 	
 	// find leftmost bit
 	ret = a; a |= a>>1; a |= a>>2; a |= a>>4; a >>= 1; a += 1;
-	
+
 	// remove it from current buffer
 	ret ^= a;
 	
