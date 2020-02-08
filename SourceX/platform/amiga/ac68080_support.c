776601ff8d79d934febf28c3d03f56ef232c0435
Samuel-DEVULDER <samuel.devulder@laposte.net>
2020-02-08 14:50:35 +0100

    reformat source code a bit

--- SourceX/platform/amiga/ac68080_support.c
+++ SourceX/platform/amiga/ac68080_support.c
@@ -50,7 +50,7 @@ extern SDL_Surface *pal_surface;
 // #define PressEscKey _ZN3dvl11PressEscKeyEv
 // extern int PressEscKey(void);
 
-#define gamemenu_quit_game	_ZN3dvl18gamemenu_quit_gameEi
+#define gamemenu_quit_game  _ZN3dvl18gamemenu_quit_gameEi
 extern void gamemenu_quit_game(int);
 
 // #define mainmenu_restart_repintro _ZN3dvl25mainmenu_restart_repintroEv
@@ -88,71 +88,71 @@ extern struct IntuitionBase *IntuitionBase;
 /*****************************************************************************/
 /* stack requirements */
 
-#define MINSTACK (128*1024)			/* 128kb */
+#define MINSTACK (128*1024)         /* 128kb */
 #ifdef __SASC
-__near								/* sas/c */
+__near                              /* sas/c */
 #endif
-size_t __stack = MINSTACK;        	/* ixemul, vbcc */
+size_t __stack = MINSTACK;          /* ixemul, vbcc */
 
 /*****************************************************************************/
 /* malloc replacement */
 
-#define SANITY_CHK			0
-#define REPLACE_SYS_MALLOC	1
+#define SANITY_CHK          0
+#define REPLACE_SYS_MALLOC  1
 
 #define USE_DL_PREFIX
 
-#define lower_malloc		__real_malloc
-#define	lower_free			__real_free
+#define lower_malloc        __real_malloc
+#define lower_free          __real_free
 extern void *lower_malloc(size_t);
 extern void  lower_free(void *);
 
-#define HAVE_MORECORE 		0
+#define HAVE_MORECORE       0
 
-#define HAVE_MMAP			1
-#define HAVE_MUNMAP			1
-#define MMAP_CLEARS			0
-#define HAVE_MREMAP 		0
+#define HAVE_MMAP           1
+#define HAVE_MUNMAP         1
+#define MMAP_CLEARS         0
+#define HAVE_MREMAP         0
 #define LACKS_SYS_MMAN_H
 
-#define MMAP				my_mmap
-#define MUNMAP				my_munmap	
-#define DIRECT_MMAP			MMAP
+#define MMAP                my_mmap
+#define MUNMAP              my_munmap
+#define DIRECT_MMAP         MMAP
 
 static void* MMAP(size_t len)
 {
-	void *p  = lower_malloc(len+4); // +1 to avoid contiguous
+    void *p  = lower_malloc(len+4); // +1 to avoid contiguous
 #if SANITY_CHK
-	if(p) {
-		ULONG *q = p;
-		*q = q;
-		p = ++q;
-	}
-	printf("MMAP(%d) = %p\n", len, p);
+    if(p) {
+        ULONG *q = p;
+        *q = q;
+        p = ++q;
+    }
+    printf("MMAP(%d) = %p\n", len, p);
 #endif
-	if(!p) {
-		printf("Out of memory (%d bytes requested).\n", len+4);
-		dlmalloc_stats();
-	}
-	return p;
+    if(!p) {
+        printf("Out of memory (%d bytes requested).\n", len+4);
+        dlmalloc_stats();
+    }
+    return p;
 }
 
 static int MUNMAP(void *p, size_t len)
 {
 #if SANITY_CHK
-	printf("MUNMAP(%p, %d)\n", p, len);
-	if(p) {
-		ULONG *q = p; --q;
-		if(*q == q) p = q;
-		else {
-			errno = EINVAL;
-			printf("Not MMAP!\n");
-			return -1;
-		}
-	}
+    printf("MUNMAP(%p, %d)\n", p, len);
+    if(p) {
+        ULONG *q = p; --q;
+        if(*q == q) p = q;
+        else {
+            errno = EINVAL;
+            printf("Not MMAP!\n");
+            return -1;
+        }
+    }
 #endif
-	lower_free(p);
-	return 0;
+    lower_free(p);
+    return 0;
 }
 
 #include "malloc.c"
@@ -160,45 +160,45 @@ static int MUNMAP(void *p, size_t len)
 void *__wrap_malloc(size_t size)
 {
 #if REPLACE_SYS_MALLOC
-	return dlmalloc(size);
+    return dlmalloc(size);
 #else
-	return __real_malloc(size);
+    return __real_malloc(size);
 #endif
 }
 void __wrap_free(void *ptr)
 {
 #if REPLACE_SYS_MALLOC
-	dlfree(ptr);
+    dlfree(ptr);
 #else
-	__real_free(ptr);
+    __real_free(ptr);
 #endif
 }
 
 void *__wrap_realloc(void *ptr, size_t size)
 {
 #if REPLACE_SYS_MALLOC
-	return dlrealloc(ptr, size);
+    return dlrealloc(ptr, size);
 #else
-	extern void *__real_realloc(void *ptr, size_t size);
-	return __real_realloc(ptr,size);
+    extern void *__real_realloc(void *ptr, size_t size);
+    return __real_realloc(ptr,size);
 #endif
 }
 
 void *__wrap_calloc(size_t num, size_t size)
 {
 #if REPLACE_SYS_MALLOC
-	return dlcalloc(num, size);
+    return dlcalloc(num, size);
 #else
-	extern void *__real_calloc(size_t num, size_t size);
-	return __real_calloc(num,size);
+    extern void *__real_calloc(size_t num, size_t size);
+    return __real_calloc(num,size);
 #endif
 }
 
 /*****************************************************************************/
 
 void assert_fail (char *file, int line, char *exp) {
-	printf("Assertion failed: %s\nFile: %s\nLine: %d\n",exp,file,line);
-	while(1) Delay(50);
+    printf("Assertion failed: %s\nFile: %s\nLine: %d\n",exp,file,line);
+    while(1) Delay(50);
 }
 
 /*****************************************************************************/
@@ -206,7 +206,7 @@ void assert_fail (char *file, int line, char *exp) {
 static void stop(void)
 {
     if(bufmem) {
-		dlfree(bufmem);
+        dlfree(bufmem);
         bufmem_roll = bufmem = NULL;
     }
 }
@@ -215,16 +215,16 @@ static void start(void)
 {
     started = 255;
     atexit(stop);
-	
+
     if (SysBase->AttnFlags &(1 << 10)) {
         ac68080_saga = _ZN3dvl10fullscreenE ? 255:0; // disable if not fullscreen
 
         bufmem = dlmemalign(32/* byte alignment for saga */, 3*FRAME_BUFFER_SZ);
-		if(bufmem) {
-			bufmem_roll = bufmem + 2*FRAME_BUFFER_SZ;
-		} else {
-			ac68080_saga = 0;
-		}
+        if(bufmem) {
+            bufmem_roll = bufmem + 2*FRAME_BUFFER_SZ;
+        } else {
+            ac68080_saga = 0;
+        }
 
         if(!VampireBase) VampireBase = OpenResource( V_VAMPIRENAME );
         if(VampireBase && VampireBase->lib_Version >= 45 &&
@@ -247,95 +247,95 @@ static void start(void)
     }
 }
 
-SDL_Surface* vampire_MakeTripleBuffer(SDL_Surface *surf) 
+SDL_Surface* vampire_MakeTripleBuffer(SDL_Surface *surf)
 {
-	if(!started) start();
-	
-	if(ac68080_saga
-	&&  surf->w==BUFFER_WIDTH
-	&&  surf->h==BUFFER_HEIGHT 
-	&&  surf->pitch==BUFFER_WIDTH
-	) {
-		surf->flags |= SDL_PREALLOC;
-		SDL_free(surf->pixels);
-		surf->pixels = bufmem;
-	} else ac68080_saga = 0;
-	return surf;
-}												
+    if(!started) start();
+
+    if(ac68080_saga
+    &&  surf->w==BUFFER_WIDTH
+    &&  surf->h==BUFFER_HEIGHT
+    &&  surf->pitch==BUFFER_WIDTH
+    ) {
+        surf->flags |= SDL_PREALLOC;
+        SDL_free(surf->pixels);
+        surf->pixels = bufmem;
+    } else ac68080_saga = 0;
+    return surf;
+}
 
 static __attribute__((noinline)) void doChkSignals(void)
 {
-	static UBYTE closing;
-	ULONG signal = SetSignal(0,0);
-	if(closing) {
-		SDL_Event sdlevent;
-		sdlevent.type = SDL_KEYDOWN;
-		sdlevent.key.keysym.sym = SDLK_ESCAPE;
-		SDL_PushEvent(&sdlevent);
-		sdlevent.type = SDL_KEYUP;
-		sdlevent.key.keysym.sym = SDLK_ESCAPE;
-		SDL_PushEvent(&sdlevent);
-	}
-	if(signal & SIGBREAKF_CTRL_E) {
-		time_t t;
-		SetSignal(0, SIGBREAKF_CTRL_E);
+    static UBYTE closing;
+    ULONG signal = SetSignal(0,0);
+    if(closing) {
+        SDL_Event sdlevent;
+        sdlevent.type = SDL_KEYDOWN;
+        sdlevent.key.keysym.sym = SDLK_ESCAPE;
+        SDL_PushEvent(&sdlevent);
+        sdlevent.type = SDL_KEYUP;
+        sdlevent.key.keysym.sym = SDLK_ESCAPE;
+        SDL_PushEvent(&sdlevent);
+    }
+    if(signal & SIGBREAKF_CTRL_E) {
+        time_t t;
+        SetSignal(0, SIGBREAKF_CTRL_E);
 
-		t = time(0);
-		printf("\nMemory statistics on %s", ctime(&t));
-		dlmalloc_stats();
-		printf("\n");
-	}
-	if(signal & SIGBREAKF_CTRL_C) {
-		SetSignal(0, SIGBREAKF_CTRL_C);
+        t = time(0);
+        printf("\nMemory statistics on %s", ctime(&t));
+        dlmalloc_stats();
+        printf("\n");
+    }
+    if(signal & SIGBREAKF_CTRL_C) {
+        SetSignal(0, SIGBREAKF_CTRL_C);
 
-		printf("Ctrl-C received\n");
-		gamemenu_quit_game(0);
-		closing = 255;
-	}
+        printf("Ctrl-C received\n");
+        gamemenu_quit_game(0);
+        closing = 255;
+    }
 }
 
 static void chkSignals(void)
 {
-	static UBYTE closing, ctr;
-	if(!ctr) {
-		ctr = 4;
-		doChkSignals();
-	} else {
-		--ctr;
-	}
+    static UBYTE closing, ctr;
+    if(!ctr) {
+        ctr = 4;
+        doChkSignals();
+    } else {
+        --ctr;
+    }
 }
 
 static void blitRect(UBYTE *dst, UBYTE *src, UWORD x, UWORD y, size_t w, UWORD h)
 {
-	src += SCREENXY(x,y);
-	dst += SCREENXY(x,y);
-	memcpy(dst-x, src-x, BUFFER_WIDTH*h);
-	
-	// do {
-		// memcpy(dst, src, w);
-		// src += BUFFER_WIDTH;
-		// dst += BUFFER_WIDTH;
-	// } while(--h);
+    src += SCREENXY(x,y);
+    dst += SCREENXY(x,y);
+    memcpy(dst-x, src-x, BUFFER_WIDTH*h);
+
+    // do {
+        // memcpy(dst, src, w);
+        // src += BUFFER_WIDTH;
+        // dst += BUFFER_WIDTH;
+    // } while(--h);
 }
 
 // check if palette has changed
 static void doPalette(void)
 {
-	static int last_version = 0;
-	if(last_version!=pal_palette_version) {
-		last_version = pal_palette_version;
-		SDL_SetColors(SDL_GetVideoSurface(), pal_palette->colors, 0, pal_palette->ncolors);
-	}
+    static int last_version = 0;
+    if(last_version!=pal_palette_version) {
+        last_version = pal_palette_version;
+        SDL_SetColors(SDL_GetVideoSurface(), pal_palette->colors, 0, pal_palette->ncolors);
+    }
 }
 
 // set saga regs to dsplay screen at ptr (modulo bytes to skip at end of line)
 static void setFrameBufferRegs(UBYTE *ptr, UWORD modulo)
 {
-	volatile UBYTE **dpy = (UBYTE**)0xDFF1EC; /* Frame buffer address */
+    volatile UBYTE **dpy = (UBYTE**)0xDFF1EC; /* Frame buffer address */
     volatile UWORD  *mod = (UBYTE**)0xDFF1E6; /* Frame buffer modulo */
 
-	*dpy = ptr;
-	*mod = modulo;
+    *dpy = ptr;
+    *mod = modulo;
 }
 
 static void doFlip(void)
@@ -343,7 +343,7 @@ static void doFlip(void)
     struct Screen *first_screen;
 
 #if DIRTY
-	// hacky way to debug
+    // hacky way to debug
     *dpy = (void*)(~31&(int)pal_surface->pixels);
     return;
 #endif
@@ -359,57 +359,57 @@ static void doFlip(void)
     if(first_screen == game_screen)
 #endif
     {
-		UBYTE *ptr = pal_surface->pixels;
-		LONG   dlt = ptr == bufmem_roll ? -2*FRAME_BUFFER_SZ : FRAME_BUFFER_SZ;
+        UBYTE *ptr = pal_surface->pixels;
+        LONG   dlt = ptr == bufmem_roll ? -2*FRAME_BUFFER_SZ : FRAME_BUFFER_SZ;
 
-		setFrameBufferRegs(ptr + SCREENXY(0,0), BUFFER_WIDTH - SCREEN_WIDTH);
+        setFrameBufferRegs(ptr + SCREENXY(0,0), BUFFER_WIDTH - SCREEN_WIDTH);
 
 #if ROLL_PTR
-	// printf("ptr=%p // %p %p %p %p\n", ptr,bufmem, bufmem+FRAME_BUFFER_SZ, bufmem+2*FRAME_BUFFER_SZ, bufmem+3*FRAME_BUFFER_SZ);
+    // printf("ptr=%p // %p %p %p %p\n", ptr,bufmem, bufmem+FRAME_BUFFER_SZ, bufmem+2*FRAME_BUFFER_SZ, bufmem+3*FRAME_BUFFER_SZ);
 
-		// need to copy parts of previous screen?
+        // need to copy parts of previous screen?
         if(copy_previous)
-		{
-			--copy_previous;
-			if(copy_all)	blitRect(ptr+dlt, ptr, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-			else			blitRect(ptr+dlt, ptr, PANEL_LEFT, PANEL_TOP, PANEL_WIDTH, PANEL_HEIGHT);
-		}
+        {
+            --copy_previous;
+            if(copy_all)    blitRect(ptr+dlt, ptr, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+            else            blitRect(ptr+dlt, ptr, PANEL_LEFT, PANEL_TOP, PANEL_WIDTH, PANEL_HEIGHT);
+        }
 
         // advance ptr
-		pal_surface->pixels = (ptr += dlt);		
+        pal_surface->pixels = (ptr += dlt);
 #endif
     }
-	doPalette();
+    doPalette();
 }
 
 int vampire_Flip(const SDL_Surface* surf)
 {
-	chkSignals();
+    chkSignals();
 
-	if(last_was_pal) {
-		last_was_pal = 0;
-		doFlip();
-	} else {
-		return SDL_Flip(surf);
-	}
+    if(last_was_pal) {
+        last_was_pal = 0;
+        doFlip();
+    } else {
+        return SDL_Flip(surf);
+    }
 }
 
 int vampire_BlitSurface(SDL_Surface *src, SDL_Rect *srcRect,
                         SDL_Surface *dst, SDL_Rect *dstRect)
 {
-	if(ac68080_saga && src==pal_surface) {
-		last_was_pal = 255;
-		if(!srcRect || srcRect->h>=SCREEN_HEIGHT) {
-			copy_all = 255;
-			copy_previous = 3;
-		} else if(srcRect->y + srcRect->h > PANEL_Y // something drawn in panel
-			&& !(srcRect->w==288 && srcRect->h==60) // ignore descpane
-		) {
-			copy_all = 0;
-			copy_previous = 3;
-		}
-		return 0;
-	} else return SDL_BlitSurface(src, srcRect, dst, dstRect);
+    if(ac68080_saga && src==pal_surface) {
+        last_was_pal = 255;
+        if(!srcRect || srcRect->h>=SCREEN_HEIGHT) {
+            copy_all = 255;
+            copy_previous = 3;
+        } else if(srcRect->y + srcRect->h > PANEL_Y // something drawn in panel
+            && !(srcRect->w==288 && srcRect->h==60) // ignore descpane
+        ) {
+            copy_all = 0;
+            copy_previous = 3;
+        }
+        return 0;
+    } else return SDL_BlitSurface(src, srcRect, dst, dstRect);
 }
 
 #define min(a,b) ((a)<=(b)?(a):(b))
@@ -460,12 +460,12 @@ int simple_BlitSurface(SDL_Surface *src, SDL_Rect *srcRect,
 extern int __real_SDL_VideoInit (const char *driver_name, Uint32 flags);
 int __wrap_SDL_VideoInit (const char *driver_name, Uint32 flags)
 {
-	ULONG bak = SysBase->AttnFlags; 
-	int ret;
-	
-	SysBase->AttnFlags &= ~(1 << 10);
-	ret = __real_SDL_VideoInit(driver_name, flags);
-	SysBase->AttnFlags = bak;
-	
-	return ret;
+    ULONG bak = SysBase->AttnFlags;
+    int ret;
+
+    SysBase->AttnFlags &= ~(1 << 10);
+    ret = __real_SDL_VideoInit(driver_name, flags);
+    SysBase->AttnFlags = bak;
+
+    return ret;
 }
